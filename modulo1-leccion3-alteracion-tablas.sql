692465dfecd3b34c0aef8c9c4cd635288d1d24bf
MaitaneP <portillamaitane@gmail.com>
2023-02-24 09:36:49 +0100

    Enunciado sql preparado

---
+++ modulo1-leccion3-alteracion-tablas.sql
@@ -0,0 +1,24 @@
+/*ENUNCIADO
+Seguimos trabajando con la BBDD que creamos en la sesión de pair programming anterior.
+Revisando nuestras tablas nos hemos dado cuenta que algunas tienen algunos errores.
+En algunas tablas nos faltan columnas, en otras hemos introducido columnas de más o incluso nos hemos equivocado a la hora de especificar el tipo de los datos.*/
+
+-- Nos aseguramos de trabajar en el schema adecuado:
+USE tienda_zapatillas;
+
+/*ACTIVIDADES
+En este ejercicio vamos a corregir los errores que hemos encontrado en nuestras tablas. */
+
+-- Tabla Zapatillas: Se nos ha olvidado introducir la marca y la talla de las zapatillas que tenemos en nuestra BBDD. Por lo tanto, debemos incluir:
+	-- marca: es una cadena de caracteres de longitud máxima de 45 caracteres, no nula.
+	-- talla: es un entero, no nulo.
+    
+-- Tabla Empleados
+	-- salario: es un entero, no nulo. Pero puede que el salario de nuestros empleados tenga decimales, por lo que le cambiaremos el tipo a decimal.
+
+-- Tabla Clientes
+	-- pais: la hemos incluido en la tabla pero nuestro negocio solo distribuye a España, por lo que es una columna que no hará falta. La eliminaremos.
+	-- codigo_postal: es un string, pero esto no tiene mucho ya que son números de longitud fija de 5 caracteres. Por lo tanto, cambiaremos el tipo a entero de longitud 5.
+
+-- Tabla Facturas:
+	-- total: madre mía!!! Se nos ha olvidado incluir el total de la cada factura generada😨!Creemos esa columna con un tipo de datos decimal.
