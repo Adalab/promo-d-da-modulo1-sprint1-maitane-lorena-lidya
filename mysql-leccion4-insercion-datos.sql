a5d0d9659ceb55f27c26b1a9894c939f9e20d4bf
Lidya <lidyadescalssamper@gmail.com>
2023-02-28 00:17:39 +0100

    Archivo SQL leccion 4 creado

---
+++ mysql-leccion4-insercion-datos.sql
@@ -0,0 +1,23 @@
+
+
+#Actividades
+
+# 1. Lo primero que vamos a hacer es insertar datos en nuestra BBDD con los siguientes datos (EXTRAER DATOS DEL GITBOOK):
+
+
+# 2. De nuevo nos hemos dado cuenta que hay algunos errores en la inserción de datos. En este ejercicios los actualizaremos para que nuestra BBDD este perfectita.
+
+	# Tabla zapatillas
+	# En nuestra tienda no vendemos zapatillas Rosas... ¿Cómo es posible que tengamos zapatillas de color rosa? 🤔 En realidad esas zapatillas son amarillas.
+
+
+	# Tabla empleados
+	# Laura se ha cambiado de ciudad y ya no vive en Alcobendas, se fue cerquita del mar, ahora vive en A Coruña.
+
+
+	# Tabla clientes
+	# El Numero de telefono de Monica esta mal!!! Metimos un digito de más. En realidad su número es: 123456728
+
+
+	# Tabla facturas
+	# El total de la factura de la zapatilla cuyo id es 2 es incorrecto. En realidad es: 89,91.
